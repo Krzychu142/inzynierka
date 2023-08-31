c3ca47a282e019b25fbb26819ca6f9c9ba550ce9
Krzysztof Radzięta <kustosz142@gmail.com>
2023-08-31 21:52:35 +0200

    home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Home from './components/Home';; 
+import React from "react";
+import Home from "./components/Home/Home";
+import { ConfigProvider } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <>
-      <Home />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#514AA8",
+            colorTextLightSolid: "#514AA8",
+            colorBorder: "#A94A8F",
+            colorPrimaryHover: "#72DBDB",
+            fontSize: 16,
+            colorTextDescription: "#A94A8F",
+          },
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
