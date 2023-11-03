076f547a8c8a87003bc17442d91e6daa84a26763
Krzysztof Radzięta <kustosz142@gmail.com>
2023-11-03 22:27:37 +0100

    created some base of clients listing

---
+++ client/src/types/client.interface.ts
@@ -0,0 +1,23 @@
+export enum Priority { 
+    VIP = "vip",
+    IMPORTANT = "important",
+    NORMAL = "normal",
+    LOWPRIORITY = "low priority"
+}
+
+export interface IClient {
+    _id: string
+    name: string
+    surname: string
+    addedAt: Date
+    email: string
+    address: string
+    shippingAddress?: string | null
+    city: string
+    country: string
+    postalCode: string
+    phoneNumber: string
+    description?: string | null
+    priority: Priority,
+    regular: boolean
+}
--- client/src/views/clients/ClientsListing.tsx
+++ client/src/views/clients/ClientsListing.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { useGetAllClientsQuery } from "../../features/clientsSlice";
 import LoadingSpinner from "../../components/loading/LoadingSpinner";
-import { List, Result } from "antd";
+import { Avatar, List, Result } from "antd";
 import useWindowWidth from "../../customHooks/useWindowWidth";
+import { IClient } from "../../types/client.interface";
+import { UserOutlined } from "@ant-design/icons";
+import "./clientsListing.css";
+import dayjs from "dayjs";
 
 const ClientsListing = () => {
   const {
@@ -28,19 +32,69 @@ const ClientsListing = () => {
           subTitle="Please try later"
         ></Result>
       )}
-      {/* {clients && (
-        <section className="employees-listing">
+      {clients && (
+        <section className="clients-listing">
           <List
             itemLayout={windowWidth > 700 ? "horizontal" : "vertical"}
             loading={isLoading}
-            dataSource={filteredData}
+            dataSource={clients}
             pagination={{
               align: "center",
               pageSize: 3,
             }}
+            renderItem={(client: IClient) => {
+              return (
+                <List.Item key={client._id}>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar
+                        size={64}
+                        shape="square"
+                        icon={<UserOutlined />}
+                      />
+                    }
+                    title={client.name + " " + client.surname}
+                    description={
+                      <span>
+                        Priority: <b>{client.priority}</b>
+                      </span>
+                    }
+                  />
+                  <ul className="clients-listing__list">
+                    <li>
+                      <b className="break-word">{client.email}</b>
+                    </li>
+                    {client.description && (
+                      <li>
+                        <p className="client__description">
+                          {client.description}
+                        </p>
+                      </li>
+                    )}
+                    <li>
+                      Added at: {dayjs(client.addedAt).format("DD.MM.YYYY")}
+                    </li>
+                    {/* maybe here how many orders for this client, end his last order or click to generate his all orders? */}
+                    {client.priority && (
+                      <li>
+                        <b className="darker">Regular client</b>
+                      </li>
+                    )}
+                    <li>Phon number: {client.phoneNumber}</li>
+                    <li>
+                      Address: {client.country} {client.city}{" "}
+                      {client.postalCode}
+                    </li>
+                    {client.shippingAddress && (
+                      <li>Shipping address: {client.shippingAddress}</li>
+                    )}
+                  </ul>
+                </List.Item>
+              );
+            }}
           ></List>
         </section>
-      )} */}
+      )}
     </>
   );
 };
