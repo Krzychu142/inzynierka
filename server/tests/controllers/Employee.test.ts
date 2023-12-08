8682a8ad0971481aaf43d027d721edf36bb2c19e
Krzysztof Radzięta <kustosz142@gmail.com>
2023-12-08 23:14:06 +0100

    added better comments for tests

--- server/tests/controllers/Employee.test.ts
+++ server/tests/controllers/Employee.test.ts
@@ -8,45 +8,50 @@ jest.mock('../../app/services/Employee.service')
 jest.mock('../../app/utils/helpers/ErrorsHandlers')
 
 describe('EmployeeController.deleteEmployee', () => {
-  it('should throw an error if the email parameter is missing', async () => {
-    const req = {
-      body: {},
-    } as unknown as Request
-    const res = {} as Response
-    res.status = jest.fn().mockReturnThis()
-    res.json = jest.fn()
+  describe('given no email parameter in the request', () => {
+    it('should throw an error if the email parameter is missing', async () => {
+      const req = {
+        body: {},
+      } as unknown as Request
+      const res = {} as Response
+      res.status = jest.fn().mockReturnThis()
+      res.json = jest.fn()
 
-    await EmployeeController.deleteEmployee(req, res)
-    const expectedError = new CustomError('The email parameter is missing', 400)
-    expect(ErrorsHandlers.handleCustomError).toHaveBeenCalledWith(
-      expectedError,
-      res,
-    )
+      await EmployeeController.deleteEmployee(req, res)
+      const expectedError = new CustomError(
+        'The email parameter is missing',
+        400,
+      )
+      expect(ErrorsHandlers.handleCustomError).toHaveBeenCalledWith(
+        expectedError,
+        res,
+      )
+    })
   })
-})
 
-describe('EmployeeController.deleteEmployee', () => {
-  it('should throw an error if the employee is not found', async () => {
-    const req = {
-      body: { email: 'test@example.com' },
-    } as unknown as Request
-    const res = {} as Response
-    res.status = jest.fn().mockReturnThis()
-    res.json = jest.fn()
+  describe('given an email that does not correspond to any employee in the database', () => {
+    it('should throw an error if the employee is not found', async () => {
+      const req = {
+        body: { email: 'test@example.com' },
+      } as unknown as Request
+      const res = {} as Response
+      res.status = jest.fn().mockReturnThis()
+      res.json = jest.fn()
 
-    EmployeeService.deleteSingleEmployee = jest
-      .fn()
-      .mockResolvedValue({ deletedCount: 0 })
+      EmployeeService.deleteSingleEmployee = jest
+        .fn()
+        .mockResolvedValue({ deletedCount: 0 })
 
-    await EmployeeController.deleteEmployee(req, res)
+      await EmployeeController.deleteEmployee(req, res)
 
-    expect(EmployeeService.deleteSingleEmployee).toHaveBeenCalledWith(
-      'test@example.com',
-    )
-    const expectedError = new CustomError('Employee not found', 404)
-    expect(ErrorsHandlers.handleCustomError).toHaveBeenCalledWith(
-      expectedError,
-      res,
-    )
+      expect(EmployeeService.deleteSingleEmployee).toHaveBeenCalledWith(
+        'test@example.com',
+      )
+      const expectedError = new CustomError('Employee not found', 404)
+      expect(ErrorsHandlers.handleCustomError).toHaveBeenCalledWith(
+        expectedError,
+        res,
+      )
+    })
   })
 })
